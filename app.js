130d6325a289c56a8bad15b633bf5daf00f9086d
unknown <viktoriya.simakova@gmail.com>
2018-11-17 21:12:40 +0200

    --

--- app.js
+++ app.js
@@ -4,10 +4,10 @@
         ctx = canvas.getContext("2d"),
         windowWidth = window.innerWidth,
         windowHeight = window.innerHeight,
-        maxFlakes = 500,
+        maxFlakes = windowWidth/2,
         angle = 0,
         flakesArr = [];
-
+    
     function initSnow() {
         canvas.width = windowWidth;
         canvas.height = windowHeight;
@@ -21,19 +21,6 @@
             })
         }
     };
-
-    function drawSnow() {
-        ctx.clearRect(0, 0, windowWidth, windowHeight);
-        ctx.beginPath();
-        ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
-        for(var i = 0; i < flakesArr.length; i++) {
-            var el = flakesArr[i];
-            ctx.moveTo(el.xCoordinate, el.yCoordinate);
-            ctx.arc(el.xCoordinate, el.yCoordinate, el.radius, 0, Math.PI*2, true);
-        }
-        ctx.fill();
-        updateSnow();
-    }
 
     function updateSnow() {
         angle += 0.01;
@@ -77,7 +64,37 @@
         }
     }
 
+    function drawSnow() {
+        ctx.clearRect(0, 0, windowWidth, windowHeight);
+        ctx.beginPath();
+        ctx.fillStyle = 'rgba(255, 255, 255, 0.8)';
+        for(var i = 0; i < flakesArr.length; i++) {
+            var el = flakesArr[i];
+            ctx.moveTo(el.xCoordinate, el.yCoordinate);
+            ctx.arc(el.xCoordinate, el.yCoordinate, el.radius, 0, Math.PI*2, true);
+        }
+        ctx.fill();
+        updateSnow();
+    }
+
+    function redrawSnow() {
+        windowWidth = window.innerWidth;
+        windowHeight = window.innerHeight;
+        maxFlakes = windowWidth/2;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        flakesArr = [];
+        initSnow();
+    }
 
     initSnow();
-    setInterval(drawSnow, 30);
+    var flakesInterval = setInterval(drawSnow, 50);
+
+    window.addEventListener('load', function() {
+        initSnow();
+    })
+
+    window.addEventListener('resize', function() {
+        redrawSnow();
+    })
+    
 }())
